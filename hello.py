a521c9b4eeb5462e88b3db12d5f622982d98fa45
almostdevelopr <minhalam.dev@gmail.com>
2022-05-26 15:54:42 +0530

    Adding simple debug

---
+++ hello.py
@@ -0,0 +1,12 @@
+def addthis(x,y):
+  print(f"X: {x} | Type: {type(x)} and Y: {y} | Type: {type(y)}")
+  try:
+    result=x+y
+  except TypeError:
+    print(f"The wrong type passed")
+    result = int(x)+int(y)
+  
+  print(f"Result: {result}")
+  return result
+
+print(addthis('1',3))
